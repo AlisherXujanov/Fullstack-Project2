c8f29164e16fc107ba1d0f6ac40b6aef4beb9a7d
Alisher <alisherxujanov163@gmail.com>
2024-08-03 18:53:56 +0500

    created Products Model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from PIL import Image
+import os
+import sys
 
-# Create your models here.
+# Clone of AliExpress products
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to='products/', default='products/default.jpg')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.price < 0:
+            raise ValueError("Price cannot be negative")
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+            
+    def delete(self, *args, **kwargs):
+        # DELETE ACTUAL FILE if not default.jpg
+        if self.image.name != 'products/default.jpg':
+            os.remove(os.path.join(sys.path[0], self.image.name))
+        super().delete(*args, **kwargs)
