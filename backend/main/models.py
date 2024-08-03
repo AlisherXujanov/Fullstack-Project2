609caf858feaee75fd9116a54d7f20afef5193d6
Alisher <alisherxujanov163@gmail.com>
2024-08-03 19:06:26 +0500

    delete image if product gets deleted

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,14 +1,18 @@
+import json
 from django.db import models
 from PIL import Image
 import os
 import sys
 
 # Clone of AliExpress products
+
+
 class Products(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.FloatField()
-    image = models.ImageField(upload_to='products/', default='products/default.jpg')
+    image = models.ImageField(upload_to='products/',
+                              default='products/default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -25,9 +29,18 @@ class Products(models.Model):
             output_size = (500, 500)
             img.thumbnail(output_size)
             img.save(self.image.path)
-            
+
     def delete(self, *args, **kwargs):
-        # DELETE ACTUAL FILE if not default.jpg
-        if self.image.name != 'products/default.jpg':
-            os.remove(os.path.join(sys.path[0], self.image.name))
+        # DELETE ACTUAL FILE
+        image_name = self.image.name.split('/')[-1]
+        if "default.jpg" not in image_name:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+            else:
+                print("Error: %s file not found" % self.image.path)
         super().delete(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
