86cb16c7aa9217d8e53297e3a8c6128410a21acd
Alisher <alisherxujanov163@gmail.com>
2024-08-13 19:16:39 +0500

    test global reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,7 @@ const BASE_URL = "http://127.0.0.1:8000"
 const context = createContext()
 const initialState = {
     currentUser: {},
+    counter: 0,
 }
 
 
@@ -12,6 +13,10 @@ function globalReducer(state, action) {
     switch (action.type) {
         case "SET_CURRENT_USER":
             return {...state, currentUser: action.payload}
+        case "INCREMENT_COUNTER":
+            return {...state, counter: state.counter + 1}
+        case "DECREMENT_COUNTER":
+            return {...state, counter: state.counter - 1}
         default:
             throw new Error("Unexpected action")
     }
