7dad4b8dffb8e895f773a990ed23bd5515498340
Alisher <alisherxujanov163@gmail.com>
2024-08-13 19:16:59 +0500

    delete global reducer

--- frontend/src/components/LandingPage/index.jsx
+++ frontend/src/components/LandingPage/index.jsx
@@ -1,34 +1,8 @@
 import Products from '../Products'
-import { context } from '../../store'
-import { useContext } from 'react'
-
-
-const btnStyle = {
-    margin: '10px',
-    padding: '10px',
-    backgroundColor: 'blue',
-    color: 'white',
-    borderRadius: '5px',
-    cursor: 'pointer'
-}
 
 function LandingPage(props) {
-    const state = useContext(context)
-
     return (
         <main className="landing-page-wrapper">
-            <h1>{state.counter}</h1>
-
-            <button style={btnStyle} className='btn'
-                onClick={(e) => {state.dispatch({type: "INCREMENT_COUNTER"})}
-            }>Increment</button>
-
-            <button style={btnStyle} className='btn'
-                onClick={(e) => {state.dispatch({type: "DECREMENT_COUNTER"})}
-            }>Decrement</button>
-
-            <hr />
-
 
             <Products />
         </main>
