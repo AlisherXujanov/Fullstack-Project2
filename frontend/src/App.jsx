4ea03c96143fcc7dc693ed192acb3da597512987
Alisher <alisherxujanov163@gmail.com>
2024-08-13 19:11:58 +0500

    created global context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import AllComponents from "./components/AllComponents.jsx"
 import { BrowserRouter } from "react-router-dom"
+import { context, globalReducer, initialState } from "./store"
+import { useReducer } from "react"
+
 
 function App() {
+  const [state, dispatch] = useReducer(globalReducer, initialState)
+  state.dispatch = dispatch
+
   return (
-    <BrowserRouter>
-      <AllComponents />
-    </BrowserRouter>
+    <context.Provider value={state}>
+      <BrowserRouter>
+        <AllComponents />
+      </BrowserRouter>
+    </context.Provider>
   )
 }
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,15 @@ const BASE_URL = "http://127.0.0.1:8000"
 
 
 const context = createContext()
-const initialState = {}
+const initialState = {
+    currentUser: {},
+}
+
 
 function globalReducer(state, action) {
     switch (action.type) {
-        case "...":
-            return "..."
+        case "SET_CURRENT_USER":
+            return {...state, currentUser: action.payload}
         default:
             throw new Error("Unexpected action")
     }
