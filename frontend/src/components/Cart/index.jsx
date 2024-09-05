79bc156fb55ed82cab62d8adfde20a882edd7436
Alisher <alisherxujanov163@gmail.com>
2024-09-05 21:09:41 +0500

    select items for checkout

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 
 function Cart(props) {
     const [products, setProducts] = useState([])
-    const [productCount, setProductCount] = useState(0)
+    const [selectedProducts, setSelectedProducts] = useState([])
 
     useEffect(() => {
         fetchProducts()
@@ -28,65 +28,106 @@ function Cart(props) {
             console.error(error)
         }
     }
+    function getProductCountFromLS(productID) {
+        let cart = JSON.parse(localStorage.getItem("cart") || "[]")
+        let product = cart.find(p => p.id === productID)
+        if (product) {
+            return product.count
+        }
+        return 0
+    }
+    function updateProductFromLS(productID) {
+        let cart = JSON.parse(localStorage.getItem("cart") || "[]")
+        let product = cart.find(p => p.id === productID)
+        if (product) {
+            let updatedProducts = products.map(p => {
+                if (p.id === productID) {
+                    p.added_count = product.count
+                }
+                return p
+            })
+            setProducts(updatedProducts)
+        } else {
+            let updatedProducts = products.map(p => {
+                if (p.id === productID) {
+                    p.added_count = 0
+                }
+                return p
+            })
+            setProducts(updatedProducts)
+        }
+    }
+    function removeFromCart(productID) {
+        let cart = JSON.parse(localStorage.getItem("cart") || "[]")
+        let product = cart.find(p => p.id === productID)
 
+        if (product) {
+            if (product.count > 1) {
+                let updatedProducts = cart.filter(p => p.id !== productID)
+                let updatedProduct = { ...product, count: product.count - 1 }
+                updatedProducts.push(updatedProduct)
+                localStorage.setItem("cart", JSON.stringify(updatedProducts))
+            } else if (product.count === 1) {
+                let updatedProducts = cart.filter(p => p.id !== productID)
+                localStorage.setItem("cart", JSON.stringify(updatedProducts))
+            }
+            updateProductFromLS(productID)
+        }
+    }
+    function addToCart(productID) {
+        let cart = JSON.parse(localStorage.getItem("cart") || "[]")
+        let product = cart.find(p => p.id === productID)
+        if (product) {
+            let updatedProducts = cart.filter(p => p.id !== productID)
+            let newProduct = { ...product, count: product.count + 1 }
 
-    // function updateProductFromLS(productID) {
-    //     let cart = JSON.parse(localStorage.getItem("cart") || "[]")
-    //     let product = cart.find(p => p.id === productID)
-    //     if (product) {
-    //         setProductCount(product.count)
-    //     } else {
-    //         setProductCount(0)
-    //     }
-    // }
-    // function removeFromCart(productID) {
-    //     let cart = JSON.parse(localStorage.getItem("cart") || "[]")
-    //     let product = cart.find(p => p.id === productID)
+            updatedProducts.push(newProduct)
+            localStorage.setItem("cart", JSON.stringify(updatedProducts))
+        } else {
+            let newProduct = { id: productID, count: 1 }
+            console.log(newProduct)
+            localStorage.setItem("cart", JSON.stringify([...cart, newProduct]))
+        }
+        updateProductFromLS(productID)
+    }
 
-    //     if (product) {
-    //         if (product.count > 1) {
-    //             let updatedProducts = cart.filter(p => p.id !== productID)
-    //             let updatedProduct = { ...product, count: product.count - 1 }
-    //             updatedProducts.push(updatedProduct)
-    //             localStorage.setItem("cart", JSON.stringify(updatedProducts))
-    //         } else if (product.count === 1) {
-    //             let updatedProducts = cart.filter(p => p.id !== productID)
-    //             localStorage.setItem("cart", JSON.stringify(updatedProducts))
-    //         }
-    //         updateProductFromLS()
-    //     }
-    // }
-    // function addToCart(productID) {
-    //     let cart = JSON.parse(localStorage.getItem("cart") || "[]")
-    //     let product = cart.find(p => p.id === productID)
-    //     if (product) {
-    //         let updatedProducts = cart.filter(p => p.id !== productID)
-    //         let newProduct = { ...product, count: product.count + 1 }
-
-    //         updatedProducts.push(newProduct)
-    //         localStorage.setItem("cart", JSON.stringify(updatedProducts))
-    //     } else {
-    //         let newProduct = { id: productID, count: 1 }
-    //         console.log(newProduct)
-    //         localStorage.setItem("cart", JSON.stringify([...cart, newProduct]))
-    //     }
-    //     updateProductFromLS()
-    // }
-
+    function setItemAsSelected(e, productID) {
+        let selected = e.target.checked
+        if (selected) {
+            setSelectedProducts([...selectedProducts, productID])
+        } else {
+            setSelectedProducts(selectedProducts.filter(p => p !== productID))
+        }
+    }
+    function setAllSelected(e = null) {
+        if (e && e.target.checked) {
+            setSelectedProducts(products.map(p => p.id))
+        } else {
+            setSelectedProducts([])
+        }
+    }
 
     return (
         <div className="cart-page-wrapper">
-
             <h1>Cart</h1>
-
             <div className="content">
                 <div className="orders-list-wrapper">
                     <div className="header">
                         <div className="select-all-wrapper">
-                            <input type="checkbox" name='select-all' />
+                            <div class="checkbox-wrapper-styled">
+                                <input
+                                    id="_checkbox-styled"
+                                    type="checkbox" name='select-all'
+                                    checked={products.length == selectedProducts.length}
+                                    onChange={(e) => { setAllSelected(e) }}
+                                />
+                                <label for="_checkbox-styled">
+                                    <div class="tick_mark"></div>
+                                </label>
+                            </div>
                         </div>
                         <p>
-                            Selected 2 out of 2
+                            Selected {selectedProducts.length} out of {products.length} products
                         </p>
                     </div>
                     <hr />
@@ -96,21 +137,32 @@ function Cart(props) {
                                 return (
                                     <div className="product-item" key={index}>
                                         <div className="input-wrapper">
-                                            <input type="checkbox" name='select-all' />
+                                            <div class="checkbox-wrapper-styled">
+                                                <input
+                                                    id={"_checkbox-styled" + index}
+                                                    type="checkbox"
+                                                    name='select-all'
+                                                    onChange={(e) => setItemAsSelected(e, product.id)}
+                                                    checked={selectedProducts.includes(product.id)}
+                                                />
+                                                <label for={"_checkbox-styled" + index}>
+                                                    <div class="tick_mark"></div>
+                                                </label>
+                                            </div>
                                         </div>
                                         <div className="image">
                                             <img src={product.image} alt="product" height={100} />
                                         </div>
                                         <div className="description">
-                                            <Link to="/product/1">
+                                            <Link to={"/product/" + product.id}>
                                                 {product.name}
                                                 <br />
                                                 {product.description.slice(0, 50) + " ..."}
                                             </Link>
                                             <div className="counter">
-                                                <button>🗑️</button>
-                                                {product.id}
-                                                <button>+</button>
+                                                <button onClick={(e) => { removeFromCart(product.id) }}>🗑️</button>
+                                                {product.added_count || getProductCountFromLS(product.id)}
+                                                <button onClick={(e) => { addToCart(product.id) }}>+</button>
                                             </div>
                                             <div className="price-wrapper">
                                                 <h2>13 143 406.51 UZS</h2>
@@ -121,7 +173,6 @@ function Cart(props) {
                                 )
                             })
                         }
-
                     </div>
                 </div>
             </div>
